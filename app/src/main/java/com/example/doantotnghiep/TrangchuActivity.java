eb86e5005cf30f517aeddd7c1a07743919fabbc8
nguyendinhthuan <nguyendinhthuan1698@gmail.com>
2020-02-19 16:16:37 +0700

    update

--- app/src/main/java/com/example/doantotnghiep/TrangchuActivity.java
+++ app/src/main/java/com/example/doantotnghiep/TrangchuActivity.java
@@ -1,6 +1,7 @@
 package com.example.doantotnghiep;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ import com.google.android.material.snackbar.Snackbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -27,7 +29,9 @@ import android.view.Menu;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class TrangchuActivity extends AppCompatActivity {
+import adapter.AdapterNavigation;
+
+public class TrangchuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private String matk;
@@ -35,22 +39,17 @@ public class TrangchuActivity extends AppCompatActivity {
     private NavigationView navView;
     private LinearLayout layout;
     private String[] arr;
+    private AdapterNavigation adapterNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trangchu);
+        setNavigationViewListener();
         matk = getIntent().getStringExtra("matk");
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -63,6 +62,8 @@ public class TrangchuActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
     }
 
     @Override
@@ -79,7 +80,37 @@ public class TrangchuActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+//    public void setDrawer() {
+//        adapterNavigation = new AdapterNavigation(this, R.layout.activity_navigation_item, arr);
+//        navView = (NavigationView) findViewById(R.id.nav_view);
+//        drawerLayout = findViewById(R.id.drawer_layout);
+//        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                item.setChecked(true);
+//                addEvent(item);
+//                return true;
+//            }
+//        });
+//    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
 
+            case R.id.nav_send: {
+                finish();
+                break;
+            }
+        }
+        //close navigation drawer
+        //drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 }
