e6a38fecd280c1c932f28ad8d5cd3b6e1cbb0eaf
nasa116vn <thuanphat2798@gmail.com>
2020-05-19 12:24:46 +0700

    update truyen ten tai khoan qua cac class

--- app/src/main/java/com/example/doantotnghiep/ui/QuanLyTaiKhoan/QuanLyTaiKhoanFragment.java
+++ app/src/main/java/com/example/doantotnghiep/ui/QuanLyTaiKhoan/QuanLyTaiKhoanFragment.java
@@ -1,9 +1,18 @@
 package com.example.doantotnghiep.ui.QuanLyTaiKhoan;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,12 +21,26 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.doantotnghiep.MainActivity;
 import com.example.doantotnghiep.R;
+import com.example.doantotnghiep.TaiKhoanActivity;
+
+import java.util.zip.Inflater;
+
 
 public class QuanLyTaiKhoanFragment extends Fragment {
 
     private QuanLyTaiKhoanViewModel slideshowViewModel;
-
+    private View myFragment;
+    private Activity activity;
+    private Animation animation;
+    private SQLiteDatabase data;
+    private String tentaikhoan;
+    private Button button_CapNhatTaiKhoan, button_DangXuat;
+    private TextView txt_TenTaiKhoanFrag, txt_TenNguoiDung, txt_DiaChi, txt_Email, txt_SDT;
+    private String taikhoan;
+    private SharedPreferences sharedPreferences;
+//code Thuan
 //    public View onCreateView(@NonNull LayoutInflater inflater,
 //                             ViewGroup container, Bundle savedInstanceState) {
 //        slideshowViewModel =
@@ -32,4 +55,74 @@ public class QuanLyTaiKhoanFragment extends Fragment {
 //        });
 //        return root;
 //    }
+
+//code Phat
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myFragment = inflater.inflate(R.layout.fragment_quanlytaikhoan, container, false);
+        return myFragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        activity = getActivity();
+        data = activity.openOrCreateDatabase("data.db", activity.MODE_PRIVATE, null);
+        animation = AnimationUtils.loadAnimation(getActivity(), R.anim.animation_edittext);
+
+        AnhXa();
+        CapNhatTaiKhoan();
+        DangXuat();
+
+        sharedPreferences = getActivity().getSharedPreferences("tendangnhap", Context.MODE_PRIVATE);
+        taikhoan = sharedPreferences.getString("taikhoancanchuyen","khong tim thay");
+        txt_TenTaiKhoanFrag.setText(taikhoan);
+
+    }
+
+    public void CapNhatTaiKhoan() {
+        button_CapNhatTaiKhoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), TaiKhoanActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void AnhXa()
+    {
+        button_CapNhatTaiKhoan = (Button) myFragment.findViewById(R.id.btnCapNhatTaiKhoan);
+        txt_TenTaiKhoanFrag = (TextView) myFragment.findViewById(R.id.txtTenTaiKhoan);
+        txt_TenNguoiDung = (TextView) myFragment.findViewById(R.id.txtTenNguoiDung);
+        txt_DiaChi = (TextView) myFragment.findViewById(R.id.txtDiaChi);
+        txt_Email = (TextView) myFragment.findViewById(R.id.txtEmail);
+        txt_SDT = (TextView) myFragment.findViewById(R.id.txtSoDienThoai);
+        button_DangXuat = (Button) myFragment.findViewById(R.id.btnDangXuat);
+    }
+
+    public void DangXuat()
+    {
+        button_DangXuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void LayTaiKhoan()
+    {
+
+
+//        Cursor cursor = data.query("tbltaikhoan",null,null,null,null,null,null);
+//        cursor.moveToFirst();
+
+
+
+    }
 }
