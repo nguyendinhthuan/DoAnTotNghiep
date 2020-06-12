4cd188e8e39d2bb9bbb4ffc05859880dd8234373
nguyendinhthuan <nguyendinhthuan1698@gmail.com>
2020-06-12 19:40:54 +0700

    update xong chuc nang loc lich su chuyen tien theo vi

--- app/src/main/java/com/example/doantotnghiep/ui/QuanLyVi/LichSuChuyenTienActivity.java
+++ app/src/main/java/com/example/doantotnghiep/ui/QuanLyVi/LichSuChuyenTienActivity.java
@@ -11,20 +11,25 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.example.doantotnghiep.R;
 import com.example.doantotnghiep.adapter.AdapterLichSuChuyenTien;
 import com.example.doantotnghiep.model.ArrayLichSuChuyenTien;
+import com.example.doantotnghiep.model.ArrayVi;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class LichSuChuyenTienActivity extends AppCompatActivity {
@@ -34,10 +39,15 @@ public class LichSuChuyenTienActivity extends AppCompatActivity {
     private AdapterLichSuChuyenTien adapterLichSuChuyenTien;
     private ImageButton imageButton_TroVeQuanLyVi;
     private Button button_ChonNgayLocChuyenTien, button_ChonTatCaChuyentien;
+    private Spinner spinner_LocLichSuChuyenTienTheoVi;
     private ListView listView_LichSuChuyenTien;
     private Date date;
     private SimpleDateFormat simpleDateFormatDialog;
     private SQLiteDatabase data;
+    private ArrayList<Integer> arrMaVi;
+    private ArrayList<String> arrTenVi;
+    private ArrayAdapter adapterVi;
+    private List<ArrayVi> listVi = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +64,9 @@ public class LichSuChuyenTienActivity extends AppCompatActivity {
         ChonNgayLocLichSuChuyenTien();
         setListView();
         LoadTatCaLichSuChuyenTien();
+        LoadDanhSachViLenSpinnerLoc();
+        LoadTatCaVi();
+        //LoadLichSuChuyenTienTheoSpinnerLocVi();
     }
 
     public void AnhXa() {
@@ -61,6 +74,7 @@ public class LichSuChuyenTienActivity extends AppCompatActivity {
         button_ChonNgayLocChuyenTien = (Button) findViewById(R.id.button_ChonNgayLocChuyenTien);
         button_ChonTatCaChuyentien = (Button) findViewById(R.id.button_ChonTatCaChuyenTien);
         listView_LichSuChuyenTien = (ListView) findViewById(R.id.listView_LichSuChuyenTien);
+        spinner_LocLichSuChuyenTienTheoVi = (Spinner) findViewById(R.id.spinner_LocLichSuChuyenTienTheoVi);
     }
 
     public void TroVeQuanLyVi() {
@@ -136,13 +150,59 @@ public class LichSuChuyenTienActivity extends AppCompatActivity {
         }
         cursor.close();
         adapterLichSuChuyenTien.notifyDataSetChanged();
-
     }
 
     public void LoadTatCaLichSuChuyenTien() {
         arr.clear();
         Cursor cursor = data.rawQuery("select malichsuchuyentien, tenvichuyen, tenvinhan, sotienchuyen, ngaythuchien" +
                 " from tbllichsuchuyentien where tbllichsuchuyentien.tentaikhoan = '" + taikhoan + "' ", null);
+        cursor.moveToFirst();
+        while (cursor.isAfterLast() == false) {
+            arr.add(new ArrayLichSuChuyenTien(cursor.getString(cursor.getColumnIndex("ngaythuchien")), cursor.getString(cursor.getColumnIndex("tenvichuyen")), cursor.getString(cursor.getColumnIndex("tenvinhan")), cursor.getInt(cursor.getColumnIndex("malichsuchuyentien")), cursor.getDouble(cursor.getColumnIndex("sotienchuyen"))));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        adapterLichSuChuyenTien.notifyDataSetChanged();
+    }
+
+    public void LoadDanhSachViLenSpinnerLoc() {
+        arrMaVi = new ArrayList<Integer>();
+        arrTenVi = new ArrayList<String>();
+        adapterVi= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrTenVi);
+        adapterVi.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_LocLichSuChuyenTienTheoVi.setAdapter(adapterVi);
+        spinner_LocLichSuChuyenTienTheoVi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                LoadLichSuChuyenTienTheoSpinnerLocVi();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void LoadTatCaVi() {
+        arrMaVi.clear();
+        arrTenVi.clear();
+        Cursor cursor = data.rawQuery("select * from tblvi where tentaikhoan = '" + taikhoan +"'", null);
+        cursor.moveToFirst();
+        listVi = new ArrayList<ArrayVi>();
+        while (cursor.isAfterLast() == false) {
+            arrMaVi.add(cursor.getInt(cursor.getColumnIndex("mavi")));
+            arrTenVi.add(cursor.getString(cursor.getColumnIndex("tenvi")));
+            cursor.moveToNext();
+        }
+        adapterVi.notifyDataSetChanged();
+    }
+
+    public void LoadLichSuChuyenTienTheoSpinnerLocVi() {
+        arr.clear();
+        Cursor cursor = data.rawQuery("select malichsuchuyentien, tenvichuyen, tenvinhan, sotienchuyen, ngaythuchien" +
+                " from tbllichsuchuyentien where tbllichsuchuyentien.tentaikhoan = '" + taikhoan + "' " +
+                " and tenvichuyen = '" + spinner_LocLichSuChuyenTienTheoVi.getSelectedItem().toString() + "' ", null);
         cursor.moveToFirst();
         while (cursor.isAfterLast() == false) {
             arr.add(new ArrayLichSuChuyenTien(cursor.getString(cursor.getColumnIndex("ngaythuchien")), cursor.getString(cursor.getColumnIndex("tenvichuyen")), cursor.getString(cursor.getColumnIndex("tenvinhan")), cursor.getInt(cursor.getColumnIndex("malichsuchuyentien")), cursor.getDouble(cursor.getColumnIndex("sotienchuyen"))));
