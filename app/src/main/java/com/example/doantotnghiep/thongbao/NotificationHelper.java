8e75a8ab2eb4d0cbd386eccd3faa5957afa003a2
nasa116vn <thuanphat2798@gmail.com>
2020-06-30 23:34:15 +0700

    update icon va ten app

--- app/src/main/java/com/example/doantotnghiep/thongbao/NotificationHelper.java
+++ app/src/main/java/com/example/doantotnghiep/thongbao/NotificationHelper.java
@@ -52,7 +52,7 @@ public class NotificationHelper extends ContextWrapper {
         return new NotificationCompat.Builder(getApplicationContext(), channel1ID)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.mipmap.logo)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
     }
